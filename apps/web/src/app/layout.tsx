0d945aa7450cf3d1ff1aae34b33a552e06bd00e7
Xinhe Wang <wangxinhe06@gmail.com>
2025-06-23 03:27:36 +0100

    add shadcn/ui

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+
+import { cn } from "@/lib/utils";
+import { ModeToggle } from "@/components/mode-toggle";
 
 import "./globals.css";
 
@@ -24,11 +28,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(geistSans.variable, geistMono.variable, "antialiased")}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <div className="fixed bottom-4 right-4">
+            <ModeToggle />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );

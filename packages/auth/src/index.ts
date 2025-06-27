8777486a232c241fd536e5de4347f34402bad26f
Bohan Wei <weibohan2007@gmail.com>
2025-06-28 00:45:26 +0800

    Turbopack & IDEA

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { username } from "better-auth/plugins";
 
 import { prisma } from "@oi-auth/db";
 
@@ -7,4 +8,17 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "sqlite", "mysql", ...etc
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    changeEmail: {
+      enabled: true,
+    },
+  },
+  plugins: [
+    username({
+      maxUsernameLength: 36,
+    }),
+  ],
 });
